53f2e89cae8d026925c99dfc9810f79ca415fc0d
david-villanueva-02 <david.villanueva.guzman@gmail.com>
2023-11-04 17:04:51 -0600

    pwm

--- ejercicio_ws/src/ejercicio_taller/ejercicio_taller/NodoRPi.py
+++ ejercicio_ws/src/ejercicio_taller/ejercicio_taller/NodoRPi.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32, Float32, Bool, String
+from std_msgs.msg import Float32, Bool, String
 
 class NodeName(Node):
     def __init__(self) -> None:
@@ -13,7 +13,7 @@ class NodeName(Node):
 
         # Create Subscribers
         self.subscriber_LED = self.create_subscription(Bool,"/estado_LED",self.callback_sub_LED,10)
-        self.subscriber_servo = self.create_subscription(Int32,"/pwm_servo", self.callback_sub_servo,10)
+        self.subscriber_servo = self.create_subscription(Float32,"/pwm_servo", self.callback_sub_servo,10)
 
         self.subscriber_test = self.create_subscription(String,"/topic_test",self.callback_sub_test,10)
 
@@ -21,18 +21,19 @@ class NodeName(Node):
         self.estado_LED = True
         self.distancia_sensor = 0.0
         self.estado_boton = False
-        self.pwm_servo = 0
+        self.pwms = [2.5, 7.5, 12.5]
+        self.pwm_servo = 7.5
+        self.i = 0
 
         # pins setup
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(11,GPIO.OUT) #LED
         GPIO.setup(13,GPIO.OUT) #PWM al servo 
-        self.servo = GPIO.PWM(13,100)
-        self.servo.start(100)
-        self.servo.ChangeDutyCycle(100)
+        self.servo = GPIO.PWM(13,50)
+        self.servo.start(self.pwm_servo)
 
         # Create timers
-        self.main_timer = self.create_timer(0.5, self.main_timer_callback)
+        self.main_timer = self.create_timer(2, self.main_timer_callback)
 
     # Create callback methods (subscribers and timers)
     def callback_sub_LED(self, msg):
@@ -40,22 +41,29 @@ class NodeName(Node):
         GPIO.output(11,self.estado_LED)
 
     def callback_sub_servo(self, msg):
-        pass
+        self.pwm_servo = msg.data
+        self.sevo.ChangeDutyCycle(self.pwm_servo)
 
     def callback_sub_test(self,msg):
         self.get_logger().info(msg.data)
 
     # send info to actuators
     def main_timer_callback(self):
+        # LED
         self.estado_LED = not(self.estado_LED)
         GPIO.output(11,self.estado_LED)
-
-    
+        # servo
+        self.i += 1
+        if (self.i == 3): i = 0
+        self.pwm_servo = self.pwms[self.i]
+        self.servo.ChangeDutyCycle(self.pwm_servo)
+        
 
 def main(args=None) -> None:
     rclpy.init(args=args)
     node_name= NodeName()
     rclpy.spin(node_name)
+    GPIO.cleanup()
     node_name.destroy_node()
     rclpy.shutdown()
 
