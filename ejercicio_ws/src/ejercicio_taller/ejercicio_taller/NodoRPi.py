d5c00f87dbaa85c357e357ee5934155481dbaab3
david-villanueva-02 <david.villanueva.guzman@gmail.com>
2023-11-05 15:37:03 -0600

    correccion self.pwm_servo

--- ejercicio_ws/src/ejercicio_taller/ejercicio_taller/NodoRPi.py
+++ ejercicio_ws/src/ejercicio_taller/ejercicio_taller/NodoRPi.py
@@ -43,9 +43,9 @@ class NodoRPi(Node):
 
     def callback_sub_servo(self, msg):
         accion = msg.data
-        if accion.upper() == "ABRIR": self.pwm_servo(12.5)
-        if accion.upper() == "CERRAR": self.pwm_servo(7.5)
-        if accion.upper() == "ABRIR MUCHO": self.pwm_servo(2.5)
+        if accion.upper() == "ABRIR": self.pwm_servo = 12.5
+        if accion.upper() == "CERRAR": self.pwm_servo = 7.5
+        if accion.upper() == "ABRIR MUCHO": self.pwm_servo = 2.5
         self.sevo.ChangeDutyCycle(self.pwm_servo)
 
     def main_timer_callback(self): #publicar info de los sensores 
